5c86517102c79511b269c3c1ee085316ab8caf78
LImberg Montes <limberg@LucasNost.lemont.com>
2024-03-17 11:21:01 -0400

    feat: se añadio ruta para verificacion de servidores

--- src/core/discord/discord.controller.ts
+++ src/core/discord/discord.controller.ts
@@ -1,47 +1,98 @@
-import { Controller, Get, Post, Req, Res } from '@nestjs/common';
-import { Request, Response } from 'express';
-import axios from 'axios';
+import { Controller, Get, Post, Req, Res } from '@nestjs/common'
+import { Request, Response } from 'express'
+import axios from 'axios'
 
 @Controller('discord')
 export class DiscordController {
+  private refreshToken: string
 
-    @Get('login')
-    login(@Res() res: Response) {
-        res.redirect(`https://discord.com/api/oauth2/authorize?client_id=${process.env.DISCORD_CLIENT_ID}&redirect_uri=${encodeURIComponent(process.env.DISCORD_REDIRECT_URL)}&response_type=code&scope=identify%20email`);
-    }
+  @Get('login')
+  login(@Res() res: Response) {
+    res.redirect(
+      `https://discord.com/api/oauth2/authorize?client_id=${
+        process.env.DISCORD_CLIENT_ID
+      }&redirect_uri=${encodeURIComponent(
+        process.env.DISCORD_REDIRECT_URL
+      )}&response_type=code&scope=identify%20email`
+    )
+  }
 
-    @Get()
-    async redirect(@Req() req: Request, @Res() res: Response) {
-        
-        console.log(process.env.DISCORD_CLIENT_ID)
-        console.log(process.env.DISCORD_CLIENT_SECRET)
-        console.log(process.env.DISCORD_REDIRECT_URL)
-        const code = req.query.code;
-        const data = {
-            client_id: process.env.DISCORD_CLIENT_ID,
-            client_secret: process.env.DISCORD_CLIENT_SECRET,
-            grant_type: 'authorization_code',
-            redirect_uri: process.env.DISCORD_REDIRECT_URL,
-            code: code,
-            scope: 'identify email'
-        };
-        const response = await axios.post(`https://discord.com/api/oauth2/token`, data, {
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded'
-            }
-        });
-        res.redirect(`http://localhost:3000/api/discord/welcome?token=${response.data.access_token}`);
+  @Get()
+  async redirect(@Req() req: Request, @Res() res: Response) {
+    console.log(process.env.DISCORD_CLIENT_ID)
+    console.log(process.env.DISCORD_CLIENT_SECRET)
+    console.log(process.env.DISCORD_REDIRECT_URL)
+    const code = req.query.code
+    const data = {
+      client_id: process.env.DISCORD_CLIENT_ID,
+      client_secret: process.env.DISCORD_CLIENT_SECRET,
+      grant_type: 'authorization_code',
+      redirect_uri: process.env.DISCORD_REDIRECT_URL,
+      code: code,
+      scope: 'identify email',
     }
+    const response = await axios.post(
+      `https://discord.com/api/oauth2/token`,
+      data,
+      {
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+      }
+    )
+    this.refreshToken = response.data.refresh_token
+    res.redirect(
+      `http://localhost:3000/api/discord/welcome?token=${response.data.access_token}`
+    )
+  }
 
-    @Get('welcome')
-    async welcome(@Req() req: Request, @Res() res: Response) {
-        const token = req.query.token;
-        const response = await axios.get('https://discord.com/api/users/@me', {
-            headers: {
-                'Authorization': `Bearer ${token}`
-            }
-        });
-        console.log(response.data)
-        res.send(`Welcome ${response.data.username}!`);
+  @Get('refresh')
+  async refresh(@Res() res: Response) {
+    const data = {
+      client_id: process.env.DISCORD_CLIENT_ID,
+      client_secret: process.env.DISCORD_CLIENT_SECRET,
+      grant_type: 'refresh_token',
+      refresh_token: this.refreshToken,
+      redirect_uri: process.env.DISCORD_REDIRECT_URL,
+      scope: 'identify email',
     }
+    const response = await axios.post(
+      `https://discord.com/api/oauth2/token`,
+      data,
+      {
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+      }
+    )
+    this.refreshToken = response.data.refresh_token
+    res.send(`New access token: ${response.data.access_token}`)
+  }
+
+  @Get('welcome')
+  async welcome(@Req() req: Request, @Res() res: Response) {
+    const token = req.query.token
+    const response = await axios.get('https://discord.com/api/users/@me', {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+    console.log('data', response.data)
+    console.log('refresh_token', this.refreshToken)
+    res.send(`Welcome ${response.data.username}!`)
+  }
+  @Get('guilds')
+  async guilds(@Req() req: Request, @Res() res: Response) {
+    const token = req.query.token
+    const response = await axios.get(
+      'https://discord.com/api/users/@me/guilds',
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    )
+    console.log(response.data)
+    res.send(response.data)
+  }
 }
